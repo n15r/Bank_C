4b0f5709db38b211dc3e5f9f2ec6e33414a827bf
nashunch0 <nassimoert@gmail.com>
2023-05-29 20:08:40 +0000

    v3.0 with menu

--- src/main.c
+++ src/main.c
@@ -1,33 +1,15 @@
 #include <stdio.h>
+#include "../include/fclients.h"
 #include "../include/mclients.h"
 #include <stdlib.h>
 #include <string.h>
 
 int main()
 {
-    liste my_list;
-    my_list.first = NULL;
-    my_list.last = NULL;
-
-    // Add some clients to the list
-    push(&my_list, 1, "Smith", "John", "Engineer", "1234567890");
-    push(&my_list, 2, "Johnson", "Jane", "Doctor", "9876543210");
-
-    // Test modify function
-    int client_id;
-    printf("Enter the client ID: ");
-    scanf("%d", &client_id);
-
-    modify(&my_list, client_id);
-
-    // Print the modified list
-    client_data *ptr = my_list.first;
-    while (ptr != NULL)
-    {
-        printf("ID: %d, Name: %s %s, Profession: %s, Phone: %s\n",
-               ptr->Id_client, ptr->nom, ptr->prenom, ptr->profession, ptr->Num_tel);
-        ptr = ptr->next;
-    }
-
+    srand(time(NULL));
+    liste *l;     // liste_co *l2; liste comptes
+    initListe(l); // MainMenu(l,l2);
+    MenuClients(l);
+    RevenirMenu(l);
     return 0;
 }
